2a83d7efa1d28b9282fa4650211db33dc745c04c
Liam Wu <l.wu@biometix.com>
2023-12-15 13:36:51 +1100

    hotfix: resolve test script issue

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 echo "starting test server..."
-docker compose up -d
+docker compose -f docker-compose.dev.yml up -d
 echo "waiting for server initialization..."
-sleep 5; until curl 0.0.0.0:8848/scan/info; do echo waiting && sleep 5; done
-echo "running tests..."
+sleep 10; until curl 0.0.0.0:8848/scan/info; do printf waiting... && sleep 5; done
+printf "\nrunning tests...\n"
 docker compose exec -it server python3 -m pytest tests -v
 echo "cleaning up..."
 docker compose down
